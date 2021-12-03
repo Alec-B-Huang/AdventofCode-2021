89d80e444b9a34e9d33a61dd9ed06501df7c13ae
Alec-B-Huang <67602734+Alec-B-Huang@users.noreply.github.com>
2021-12-02 17:22:50 -0800

    Add files via upload

---
+++ 002Solve.py
@@ -0,0 +1,37 @@
+file = open('C:\\AlecData\\002.txt', 'r')
+data = file.read()
+
+Horizontal = 0
+Depth = 0
+data = data.splitlines()
+for line in data:
+    newLine = line.split()
+    if newLine[0] == 'forward':
+        Horizontal += int(newLine[1])
+    elif newLine[0] == 'down':
+        Depth += int(newLine[1])
+    elif newLine[0] =='up':
+        Depth -= int(newLine[1])
+
+print('PART 1')
+print('Horizontal position:' , Horizontal)
+print('Final Depth:' , Depth)
+print('The result is:' , Depth * Horizontal)
+
+Horizontal = 0
+Depth = 0
+Aim = 0
+for line in data:
+    newLine = line.split()
+    if newLine[0] == 'forward':
+        Horizontal += int(newLine[1])
+        Depth += int(newLine[1]) * Aim
+    elif newLine[0] == 'down':
+        Aim += int(newLine[1])
+    elif newLine[0] =='up':
+        Aim -= int(newLine[1])
+
+print('PART 2')
+print('Horizontal position:' , Horizontal)
+print('Final Depth:' , Depth)
+print('The result is:' , Depth * Horizontal)
